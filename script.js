c251123b3fd7784adb0cb163dcc91310c86d7f9b
natyele <natyelebertolanii@gmail.com>
2019-07-30 18:01:59 -0300

    adicionar exercicios localstorage

---
+++ script.js
@@ -0,0 +1,49 @@
+//function sairPagina(event){
+  // console.log(`Eixo X: ${event.pageX} e Eixo Y ${event.pageY}`);
+//}
+//window.onclick = sairPagina;
+
+//function sairPagina(event){
+  // if(event.pageX >= 0 && event.pageY <=100){
+    //alert("NAO SAIA DA PAGINA POR FAVOR")//
+
+    //}else{}
+//}
+
+//window.onmousemove = sairPagina;
+
+    
+//localStorage.setItem();
+
+let caixaTexto = document.querySelector("input");
+let botao  = document.querySelector("button");
+let div = document.querySelector("div");
+let section = document.querySelector("section");
+let h2 = document.querySelector("h2");
+let a =  document.querySelector("a");
+
+if(localStorage.nome){
+  div.style.display = "none";
+  section.style.display = "initial";
+  h2.innerHTML = `SEJE BEM VINDO ${localStorage.nome}`
+  a.innerHTML = `VOCE E ${localStorage.nome} ?`;
+}else{
+    function colocarNome(){
+        section.style.display = "none";
+        localStorage.setItem("nome", caixaTexto.value);
+       if(localStorage.nome){
+        div.style.display = "nome";
+        section.style.display = "initial";
+        h2.innerHTML = `seje bem vindo ${localStorage.nome}`;
+        a.innerHTML = `voce nao e ${localStorage.nome}? `;
+
+    }
+  }
+
+}
+function resetar(){
+    localStorage.removeItem("nome");
+
+}
+a.onclick = resetar
+botao.onclick = colocarNome;
